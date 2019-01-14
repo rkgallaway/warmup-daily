5a4d875242b0ef0e6727774f0df3acfa1354eafb
rkgallaway <rkgallaway@msn.com>
2019-01-14 07:29:19 -0800

    daily-monday

---
+++ index-1-14-19.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const arr = [];
+
+for (let i = 0; i <= 10; i++){
+  arr.push(i);
+}
+
+console.log(arr);
+
+function forLoop(arr){
+  for (let i in arr){
+    console.log(arr[i]);
+  }
+}
+
+forLoop(arr);
+
+function whileLoop(arr){
+  let count = 0;
+  while (count < arr.length){
+    console.log(arr[count]);
+    count++;
+  }
+}
+
+whileLoop(arr);
+
+function myMapper(arr, cb){
+  const result = [];
+  for(let i in arr){
+    result.push(cb(arr[i]));
+  }
+  return result;
+}
+
+console.log(myMapper(arr, (value)=> {
+  return Math.floor(value / 3 / 3);
+}));
+ // not working
