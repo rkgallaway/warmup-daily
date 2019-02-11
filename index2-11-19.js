340f615773cc2521ae0a2a68984f3ff7b8d66262
rkgallaway <rkgallaway@msn.com>
2019-02-11 07:54:23 -0800

    daily

---
+++ index2-11-19.js
@@ -0,0 +1,62 @@
+'use strict';
+
+let numbers =[1,2,3,4,5,6,7,8,9,10];
+const people = ['Kookla', 'Fran', 'Olie'];
+const stuff = {
+  tv: 'huge',
+  radio: 'old',
+  toothbrush: 'frayed',
+  cars: ['Toyota', 'Mazda'],
+};
+
+//////////////////////////////////////////////////////
+function forLoop (arr) {
+  for( let i in arr) {
+    console.log(arr[i]);
+  }
+}
+
+function whileLoop(arr){
+  let array = [...arr];
+  while(array.length){
+    console.log(array.shift());
+  }
+}
+
+function map(arr, cb){
+  let newArray = [];
+  for(let i in arr){
+    newArray.push(cb(arr[i]));
+  }
+  return newArray;
+}
+
+function filter (arr, cb){
+  let newArray = [];
+  for (let i in arr){
+    if (cb(arr[i])){
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+function reduce (arr, cb, newThing){
+  for( let i in arr){
+    newThing = cb(newThing, arr[i], i);
+  }
+  return newThing;
+}
+
+const state= {people, stuff};
+let newPeople = ['Odie', ...people, 'Garfield'];
+const newStuff = {...stuff, cars: [...stuff.cars, 'Pacifica']};
+let newState = {...state, people: ['Odie', ...people, 'Garfield'],stuff: {...stuff, cars: [stuff.cars, 'Pacifica']}};
+
+console.log({people});
+console.log({newPeople});
+console.log ({stuff});
+
+console.log({newStuff});
+console.log({state});
+console.log({newState});
